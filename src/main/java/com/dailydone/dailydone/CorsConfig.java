5748397e57508d6131fbc9f70c901b451920f8f9
MahmudiIsnije <isnije01@hotmail.de>
2025-06-03 16:58:29 +0200

    Backend verbessert

--- src/main/java/com/dailydone/dailydone/CorsConfig.java
+++ src/main/java/com/dailydone/dailydone/CorsConfig.java
@@ -11,9 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*") 
+                        .allowedOrigins("*")  
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
                         .allowCredentials(false);
             }
-        }
+}
