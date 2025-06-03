cd2e5dcaf86b9a51040f5c23def8507750787611
MahmudiIsnije <isnije01@hotmail.de>
2025-06-03 15:53:20 +0200

    CORS Global komplett

--- src/main/java/com/dailydone/dailydone/CorsConfig.java
+++ src/main/java/com/dailydone/dailydone/CorsConfig.java
@@ -6,16 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+public class CorsConfig implements WebMvcConfigurer {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedMethods("*")
+                        .allowedHeaders("*") 
+                        .allowCredentials(false);
             }
-        };
-    }
-}
+        }
